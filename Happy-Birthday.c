5487dfb6e235915327892346eb426a363c1cace1
Bella Campbell <Bella.Campbell@tuta.io>
2023-05-03 21:48:11 +0100

    Split up to header file

--- Happy-Birthday.c
+++ Happy-Birthday.c
@@ -1,24 +1,4 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <time.h>
-#include <unistd.h>
-#include <string.h>
-
-#define DEFAULT_BIRTHDAY_YEAR 2006
-#define DEFAULT_BIRTHDAY_MONTH 4
-#define DEFAULT_BIRTHDAY_DAY 13
-
-#define PATH_START "/home/"
-#define PATH_START_SIZE 6
-#define PATH_END "/.config/birthday.conf"
-#define PATH_END_SIZE 22
-
-struct Birthday
-{
-    int year;
-    int month;
-    int day;
-};
+#include "Happy-Birthday.h"
 
 int main(void)
 {
